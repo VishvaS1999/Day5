a45a8e03b4b46887268c83b8f4b559f4be0c36b4
VishvaS1999 <112763485+VishvaS1999@users.noreply.github.com>
2022-09-20 18:47:26 +0530

    ifelif elase example

---
+++ ifelifelse.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+var1=10
+var2=10
+if [ $var1 -gt $var2 ]
+then
+	echo "$var2 is greater than $var1"
+
+elif [ $var2 -eq $var1 ]
+then
+	echo "Variables are equal"
+else
+	echo "$var2 is less then $var1"
+fi
+
